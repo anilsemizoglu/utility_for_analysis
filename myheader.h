24e8efb1639f35c7865fbebb0c6bb3262b17861f
anilsemizoglu <anilsemizoglu@gmail.com>
2013-11-25 12:55:57 -0800

    new file:   myheader.h

---
+++ myheader.h
@@ -0,0 +1,81 @@
+// C++
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <cstdio>
+		//NORMALIZE//
+//Normalize to normalize a histogram
+template <class T>
+T Normalize (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(100*(1/integral));}
+  return (result);
+}
+
+//Normalize2 to normalize two histograms stacked
+template <class T>
+T Normalize2 (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(50*(1/integral));}
+  return (result);
+}
+//Normalize3 to normalize two histograms stacked
+template <class T>
+T Normalize3 (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(100*(1/(3*integral)));}
+  return (result);
+}
+//Normalize4 to normalize two histograms stacked
+template <class T>
+T Normalize4 (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(25*(1/integral));}
+  return (result);
+}
+//Normalize5 to normalize two histograms stacked
+template <class T>
+T Normalize5 (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(20*(1/integral));}
+  return (result);
+}
+
+//Normalize6 to normalize two histograms stacked
+template <class T>
+T Normalize6 (T a) {
+  T result;
+	double integral = a->Integral();
+	if (integral > 0){a->Scale(100*(1/(6*integral)));}
+  return (result);
+}
+
+	//GET BIN CONTENT//
+
+	template <class get>
+	get getbin (get a, const string b) {
+	get result;
+
+	const string c = "/home/users/sanil/MT2/bincontent/" + b + ".txt";
+	const char* d = c.c_str();
+
+	ofstream file(d);
+	if(! file.is_open()){return 0;}
+	if(file.is_open()){
+	int max = a->FindLastBinAbove(0,1);
+		for (unsigned int i=1; (i <= max); i++ ){
+	//cout <<"Bin # "<< i << endl; 
+	file << a->GetBinContent(i) << endl;
+							}
+			  }
+  return (result);
+}
+
